61b0cc02933052057ef5fc69ae5bea6c8f0877d3
vnkmpf <t@example.com>
2024-02-05 16:36:35 +0100

    ci: add license checker
    - madewithlove/license-checker doesn't work with Symfony 7
    - dominikb/composer-license-checker doesn't want to work here

---
+++ .allowed-licenses.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Lendable\ComposerLicenseChecker\LicenseConfigurationBuilder;
+
+return (new LicenseConfigurationBuilder())
+    ->addLicenses(
+        'MIT',
+        'BSD-2-Clause',
+        'BSD-3-Clause',
+        'Apache-2.0',
+    )
+    ->build();
