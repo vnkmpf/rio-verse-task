7744da3a822cbf79a9227649f20910d21c04009c
vnkmpf <t@example.com>
2024-02-07 10:34:53 +0100

    refactor: CreateReservation

--- src/Reservation/Application/Controller/CreateReservation.php
+++ src/Reservation/Application/Controller/CreateReservation.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Uid\UuidV7;
 
 #[Route('/reservations', methods: ['POST'])]
@@ -29,43 +30,51 @@ final class CreateReservation extends AbstractController
 
     public function __invoke(Request $request): Response
     {
-        $user_id = $request->headers->get('X-user_id');
-        $user = $this->user_repository->getById(new UuidV7($user_id));
+        $user_id = new UuidV7($request->headers->get('X-user_id'));
+
+        try {
+            $request_data = $this->decodeJsonContent($request);
+            $event_id = new UuidV7($request_data->event_id ?? throw new BadRequestHttpException('event_id'));
+            $event = $this->event_repository->getById($event_id);
+            $reservation = match ($event?->staff_id->toRfc4122() === $user_id->toRfc4122()) {
+                true => $this->createStaffReservation($request_data, $event_id),
+                false => $this->createLoggedInUserReservation($request_data, $event_id, $user_id),
+            };
+            return new JsonResponse($reservation, 201);
+        } catch (\Exception $e) {
+            return $this->sendJsonProblem($e);
+        }
+    }
+
+    private function createStaffReservation(object $request_data, Uuid $event_id): Reservation
+    {
+        return new Reservation(
+            new UuidV7(),
+            $request_data->user_name ?? throw new BadRequestHttpException('user_name'),
+            $request_data->user_email ?? throw new BadRequestHttpException('user_email'),
+            null,
+            $event_id,
+            $request_data->description ?? throw new BadRequestHttpException('description'),
+            ReservationStatus::PENDING,
+        );
+    }
+
+    private function createLoggedInUserReservation(object $request_data, Uuid $event_id, Uuid $user_id): Reservation
+    {
+        $user = $this->user_repository->getById($user_id);
 
         if ($user === null) {
             throw new UnauthorizedHttpException('');
         }
 
-        try {
-            $request_data = $this->decodeJsonContent($request);
-            $event_id = new UuidV7($request_data->event_id ?? throw new BadRequestHttpException('event_id'));
-
-            /** @var Event $event */
-            $event = $this->event_repository->getById($event_id);
-            if ($event->staff_id->toRfc4122() === $user_id) {
-                $reservation = new Reservation(
-                    new UuidV7(),
-                    $request_data->user_name,
-                    $request_data->user_email,
-                    null,
-                    $event_id,
-                    $request_data->description ?? throw new BadRequestHttpException('description'),
-                    ReservationStatus::PENDING,
-                );
-            } else {
-                $reservation = new Reservation(
-                    new UuidV7(),
-                    $user->name,
-                    $user->email,
-                    $user->id,
-                    $event_id,
-                    $request_data->description ?? throw new BadRequestHttpException('description'),
-                    ReservationStatus::PENDING,
-                );
-            }
-            return new JsonResponse($reservation, 201);
-        } catch (\Exception $e) {
-            return $this->sendJsonProblem($e);
-        }
+        return new Reservation(
+            new UuidV7(),
+            $user->name,
+            $user->email,
+            $user->id,
+            $event_id,
+            $request_data->description ?? throw new BadRequestHttpException('description'),
+            ReservationStatus::PENDING,
+        );
     }
 }
